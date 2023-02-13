c1be9e82a78b57c13ca4300c10b01fdf5b4dfc0b
Byownies <sinmad30@gmail.com>
2023-02-13 22:46:11 +0700

    first commit

---
+++ app/Http/Controllers/EmployeeController.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Department;
+use App\Models\Employee;
+
+class EmployeeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $data=Employee::orderBy('id','desc')->get();
+        return view('employee.index',['data'=>$data]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $data=Department::orderBy('id','desc')->get();
+        return view('employee.create',['departments'=>$data]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'full_name'=>'required',
+            'address'=>'required',
+            'mobile'=>'required',
+            'status'=>'required',
+        ]);
+
+        $data=new Employee();
+        $data->department_id=$request->depart;
+        $data->full_name=$request->full_name;
+        $data->address=$request->address;
+        $data->mobile=$request->mobile;
+        $data->status=$request->status;
+        $data->save();
+
+        return redirect('employee/create')->with('msg','Data has been submitted');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $departs=Employee::orderBy('id','desc')->get();
+        $data=Employee::find($id);
+        return view('employee.edit',['departs'=>$departs,'data'=>$data]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
